b28d2d99b727bb008d5de0e4cda5238b3511a323
mattia <mattiav.dev@gmail.com>
2021-04-01 07:08:01 +0300

    · start implementation HorizontalList component (added map function to render ListCard and passing data) · created container for ListCard component

--- src/components/horizontal-scrolling-list/HorizontalList/HorizontalList.component.jsx
+++ src/components/horizontal-scrolling-list/HorizontalList/HorizontalList.component.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import ListCard from '../ListCard/ListCard.component';
 import './HorizontalList.styles.scss';
 
-const HorizontalList = () => {
+const HorizontalList = (props) => {
+  console.log(props);
+
+  if(props.err) {
+    //need to handle the error
+    console.log(`Data fetching failed with the error ${props.err}`)
+  }
+
+  if(props.isFetching) {
+    //will load a spinner - to be created
+    return(
+      <div className='spinner'>Loading data....</div>
+    )
+  }
+
+  if(props.data.length === 0) return null;
+
+  const list = props.data.results;
+
   return(
-    <div></div>
+    <div className='grid'>
+      <h2 className="hz-list__title">What's popular</h2>
+      {
+        list.map(item => <ListCard data={item} key={item.id} />)
+      }
+    </div>
   )
 }
 
---
+++ src/components/horizontal-scrolling-list/ListCard/ListCard.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ListCard.styles.scss';
+
+const ListCard = (props) => {
+  return(
+    <div></div>
+  )
+}
+
+export default ListCard;
