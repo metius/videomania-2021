72e01428bb2ff0fbc61a532d80b1f9bc72ce2f5b
mattia <mattiav.dev@gmail.com>
2021-04-01 06:29:31 +0300

    · resolved issue with HorizontalList component (typo)

--- src/components/horizontal-scrolling-list/HorizontalList/HorizontalList.component.jsx
+++ src/components/horizontal-scrolling-list/HorizontalList/HorizontalList.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './HorizontalList.styles.scss';
 
 const HorizontalList = () => {
-  return() {
+  return(
     <div></div>
-  }
+  )
 }
 
 export default HorizontalList;
