a4a4e49f2d1c38c21eddd3c5fe052ff8c1b00564
mattia <mattiav.dev@gmail.com>
2021-04-05 06:49:37 +0300

    · modified style for TMDB logo (added style for Alt Text when logo not loaded) · added filter for Card title based on TYPE (movie or Tv Show) · added StarIcon to add Item to favourites

--- src/components/ItemCard/ItemCard.component.jsx
+++ src/components/ItemCard/ItemCard.component.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
 import { getPicturePath } from '../../utils/utilities';
 import {S_BACKDROP, M_BACKDROP, L_BACKDROP} from '../../utils/tmdb_constants';
+import { TYPE_MOVIE, TYPE_TVSHOW } from '../../utils/constants';
+
+import StarIcon from '../StarIcon/StarIcon.component';
+
 import './ItemCard.styles.scss';
 
 const ItemCard = ({data, err, isError, isFetching, type}) => {
   console.log("card props", data);
+
+  if(isError) {
+    //need to handle the error
+    console.log(`Data fetching failed with the error ${err}`)
+  }
+  
+  if(isFetching) {
+    //will load a spinner - to be created
+    return(
+      <div className='spinner'>Loading data....</div>
+      )
+    }
+    
+  if(data.length === 0) return null;
+
   const backdropImg = getPicturePath(data.backdrop_path, M_BACKDROP);
+  
+  let title;
+  if( type === TYPE_MOVIE) {
+    title = data.title;
+  } else if ( type === TYPE_TVSHOW ) {
+    title = data.name;
+  } else {
+    title = ' ';
+  }
   return(
     <main>
       <article className="item-page">
         <div className="item-page__header">
           <img src={backdropImg} alt={data.name} className="item-page__img" />
-          {/* 
-          --> add to favourite ICON - to be redesign and review
-          <div className="item-page__favourite">
-            <a href="#">
-              <i className="far fa-star"></i>
-            </a>
-          </div> */}
+          <StarIcon />
           <div className="item-page__title-box">
-            <h2 className="item-page__title">{data.title}</h2>
+            <h2 className="item-page__title">{title}</h2>
           </div>
         </div>
       </article>
