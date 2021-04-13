b3b9134a8560c1927d9c5dce01c75e68de1e1777
mattia <mattiav.dev@gmail.com>
2021-04-13 06:49:12 +0300

    · created ModalVideo component (not finished) and integrate to MediaListCard component

--- src/components/horizontal-scrolling-list/MediaListCard/MediaListCard.component.jsx
+++ src/components/horizontal-scrolling-list/MediaListCard/MediaListCard.component.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
-import ListCard from '../ListCard/ListCard.component';
+import React, {Component} from 'react';
+import ModalVideo from '../../ModalVideo/ModalVideo.component';
 import {getYouTubeThumbsHD} from '../../../utils/utilities'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faYoutube} from '@fortawesome/free-brands-svg-icons';
 
-const MediaCard = (props) => {
-  console.log(props)
-  
-  //need to pass the correct link to the video
-  return(
-    <ListCard type={props.type} data={props.data} >
-      <img className="card__poster card__poster--media" src={getYouTubeThumbsHD(props.data.key)} alt={props.data.name} />
-      <div class="card__play-icon">                 
-        <FontAwesomeIcon icon={faYoutube} />
+import './MediaListCard.styles.scss';
+
+
+class MediaCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    }
+  }
+
+  render() {
+    console.log(this.props);
+    const videoId = this.props.data.key;
+    const name = this.props.data.name;
+    return(
+      <div className='card card__modal-wrapper'>
+        <ModalVideo />
+        <img className="card__poster card__poster--media" src={getYouTubeThumbsHD(videoId)} alt={name} />
+        <div class="card__play-icon">                 
+          <FontAwesomeIcon icon={faYoutube} />
+        </div>
       </div>
-    </ListCard>
-  )
+    )
+  }
 } 
+  //need to pass the correct link to the video
+//   return(
+    
+//     <div className='card'>
+//       <img className="card__poster card__poster--media" src={getYouTubeThumbsHD(props.data.key)} alt={props.data.name} />
+//       <div class="card__play-icon">                 
+//         <FontAwesomeIcon icon={faYoutube} />
+//       </div>
+//     </div>
+    
+//   )
+// } 
 
 export default MediaCard;
