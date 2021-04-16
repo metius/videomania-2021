459417a6b9fb43ff5c64cacb8773c61fe0c1baec
mattia <mattiav.dev@gmail.com>
2021-04-16 05:21:12 +0300

    · modified how to pass `channel` parameter to ModalVideo component · clean up console.log

--- src/components/horizontal-scrolling-list/MediaListCard/MediaListCard.component.jsx
+++ src/components/horizontal-scrolling-list/MediaListCard/MediaListCard.component.jsx
@@ -25,17 +25,19 @@ class MediaCard extends Component {
   }
 
   render() {
-    console.log(`State in MediaListCard --> ${this.state.isOpen}`);
     const videoId = this.props.data.key;
     const name = this.props.data.name;
     const site = this.props.data.site;
+    
 
     return(
       <div className='card card__modal-wrapper'>
-        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId={videoId} onClose={() => this.setState({isOpen: false})} />
-        <img className="card__poster card__poster--media" src={getYouTubeThumbsHD(videoId)} alt={name} onClick={this.openModal}/>
-        <div className="card__play-icon">                 
-          <FontAwesomeIcon icon={faYoutube} />
+        <ModalVideo channel={site.toLowerCase()} isOpen={this.state.isOpen} videoId={videoId} onClose={() => this.setState({isOpen: false})} />
+        <div onClick={this.openModal}>
+          <img className="card__poster card__poster--media" src={getYouTubeThumbsHD(videoId)} alt={name} />
+          <div className="card__play-icon">                 
+            <FontAwesomeIcon icon={faYoutube} />
+          </div>
         </div>
       </div>
     )
