45d2bec362c8bf92ecc143227bcbb0128d23c622
mattia <mattiav.dev@gmail.com>
2021-07-25 18:48:58 +0200

    · modified responsive images (from <picture> to <IMg> with srcset and sizes

--- src/components/horizontal-scrolling-list/ResponsiveImages/ResponsiveImages.component.jsx
+++ src/components/horizontal-scrolling-list/ResponsiveImages/ResponsiveImages.component.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import {getPicturePath} from '../../../utils/utilities';
 
+// const ResponsiveImages = ({imgPath, altDesc, defaultSize, sizeArray, cssClass}) => {
+//   return(
+//     <picture>
+//       {
+//         sizeArray.map((size) => <source media={size.mediaQuery} srcSet={getPicturePath(imgPath, size.imgSize)}/>)
+//       }
+//       <img src={getPicturePath(imgPath, defaultSize)} alt={altDesc} className={cssClass} />
+//     </picture>
+//   )
+// }
+
 const ResponsiveImages = ({imgPath, altDesc, defaultSize, sizeArray, cssClass}) => {
+  
   return(
-    <picture>
-      {
-        sizeArray.map((size) => <source media={size.mediaQuery} srcSet={getPicturePath(imgPath, size.imgSize)}/>)
+    <img 
+      src={getPicturePath(imgPath, defaultSize)}
+      alt={altDesc}
+      // width="92"
+      // height="138"
+      srcSet={ 
+        sizeArray.map((size) => `${getPicturePath(imgPath, size.imgSize)} ${size.imgSize.slice(1)}w`)}
+      className={cssClass}
+      sizes={
+        sizeArray.map((size) => `${size.mediaQuery} ${size.imgSize.slice(1)}px`)
       }
-      <img src={getPicturePath(imgPath, defaultSize)} alt={altDesc} className={cssClass} />
-    </picture>
+    />
   )
 }
 
